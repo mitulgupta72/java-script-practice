9f012873220d7782c5ca3af6646771f5ddc27744
mitulgupta72 <mitulgupta283@gmail.com>
2022-05-05 01:14:32 +0530

    firstcommit

---
+++ hello.js
@@ -0,0 +1,81 @@
+// java script console API
+// console.log("hello mitul");
+// document.write("documwnysd");
+// console.warn('this is warning');
+// console.error("this one is error");
+alert("ruko beta ")
+
+// data types
+// NUMBERS 
+// STRINGS
+// BOLLEAN
+// SymbolS
+// ARRAYS  
+
+// arithematic operators 
+// +,-,*,/,
+
+// assignmet  oprator 
+// =
+// +=
+// -=
+// *=
+// /=
+
+// comparison operator 
+// ==
+// >=
+// <=
+// >
+// <
+
+// logical operator
+// &&
+// ||
+// !
+
+// functions 
+// if else
+
+// for loop like c 
+// ya phir 
+// var arr =[1,23,3,4,5,6,8,7,9];
+// arr.forEach(function(element){
+//     console.log(element);
+// })
+// console.log("mitul bhaiya beautiful");
+
+// array methods
+// let myarr= ['mitul',52,5.56,true];
+// console.log(myarr);
+// myarr.pop();
+// console.log(myarr);
+// console.log(myarr.length);
+// myarr.push('mitul bhaiya');
+// console.log(myarr);
+// myarr.shift();
+// console.log(myarr);
+// myarr.unshift('loaua');
+// console.log(myarr);
+
+// string methods 
+// let string ="mitul bhaiya is vvery  mitul is beautiful  is usefg";
+// console.log(string.length);
+// console.log(string.indexOf('is'));
+// console.log(string.lastIndexOf("is"));
+// d= string.replace("mitul","gajju");  // replaces only firs occurence 
+// console.log(string,d);
+
+// // date methods 
+// let mydate = new Date();
+// console.log(mydate);
+// console.log(mydate.getTime());
+// console.log(mydate.getUTCFullYear());
+// console.log(mydate.getDay());
+
+
+// dom manipulation
+
+// var  n= document.getElementById('click');
+// n.innerHTML="mitul";
+// console.log(n.innerHTML);
