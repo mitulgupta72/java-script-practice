c2807c7a69df4e0038bd1f5117fc6887824f84b0
mitulgupta72 <mitulgupta283@gmail.com>
2022-10-21 14:54:08 +0530

    upload

---
+++ after_dom.js
@@ -0,0 +1,15 @@
+console.log("afterdom");
+let a=window;
+
+// alert('hello mitul ');
+// a=prompt('this will destroy your computer,type your name ');
+
+//returns   a bool value 
+// a=confirm('do you want to continue on this page or not')  
+
+// a=location;
+a=location.toString();
+// a=location.href;
+// a=history.length;
+
+console.log(a);
